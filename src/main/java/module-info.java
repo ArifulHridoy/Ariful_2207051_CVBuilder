d30a3d03c907d186e23a432943c131d3f052f6f2
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-11-09 17:45:51 +0600

    Initialize git repo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.sheikh_2207051_cvbuilder {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.sheikh_2207051_cvbuilder to javafx.fxml;
+    exports com.example.sheikh_2207051_cvbuilder;
+}
