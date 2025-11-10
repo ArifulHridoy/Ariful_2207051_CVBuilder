5fe648d363771a6862fb13fadeb3fdc41454f2a0
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-11-11 01:17:47 +0600

    Creating all java and fxml files

---
+++ src/main/java/com/example/sheikh_2207051_cvbuilder/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.sheikh_2207051_cvbuilder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
