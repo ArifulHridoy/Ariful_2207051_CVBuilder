148c08f7ea0b10fd7be875bbb02243b93603e350
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-11-11 21:21:44 +0600

    Files are set in folder for a organized way coding & made Home.fxml using scene builder

--- src/main/java/com/example/sheikh_2207051_cvbuilder/controllers/Launcher.java
+++ src/main/java/com/example/sheikh_2207051_cvbuilder/controllers/Launcher.java
@@ -1,9 +1,10 @@
 package com.example.sheikh_2207051_cvbuilder;
 
+import com.example.sheikh_2207051_cvbuilder.Main;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(Main.class, args);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,10 @@
 module com.example.sheikh_2207051_cvbuilder {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
+    opens com.example.sheikh_2207051_cvbuilder.controllers to javafx.fxml;
+    opens com.example.sheikh_2207051_cvbuilder.model to javafx.fxml;
     opens com.example.sheikh_2207051_cvbuilder to javafx.fxml;
+
     exports com.example.sheikh_2207051_cvbuilder;
 }
