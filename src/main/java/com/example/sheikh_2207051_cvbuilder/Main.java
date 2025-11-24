0aac2b94eedbe312778b468afece05be73ec4962
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-11-24 23:27:44 +0600

    Tried and succesffully added concurrancy package in my main method

--- src/main/java/com/example/sheikh_2207051_cvbuilder/Main.java
+++ src/main/java/com/example/sheikh_2207051_cvbuilder/Main.java
@@ -1,18 +1,38 @@
 package com.example.sheikh_2207051_cvbuilder;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main extends Application
 {
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
     @Override public void start(Stage primaryStage) throws Exception{
-        Parent root=FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
-        primaryStage.setTitle("CV Builder");
-        Scene scene=new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        executor.submit(()->{
+            try{
+                Parent root=FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
+                Platform.runLater(()->{
+                    primaryStage.setTitle("CV Builder");
+                    Scene scene = new Scene(root);
+                    primaryStage.setScene(scene);
+                    primaryStage.show();
+                });
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        });
     }
-    public static void main(String[] args) {launch(args);}
+
+    @Override public void stop() throws Exception{
+        executor.shutdown();
+        super.stop();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
